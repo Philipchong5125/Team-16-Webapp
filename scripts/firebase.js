6cd929810bff8209c8b81f3dc58f421a68428efa
Philip Chong <pchong28@my.bcit.ca>
2021-03-16 22:24:23 -0700

    Removed reminder confirmation, added database connection

---
+++ scripts/firebase.js
@@ -0,0 +1,27 @@
+// // Your web app's Firebase configuration
+// var firebaseConfig = {
+//     apiKey: "AIzaSyB-Zz3DmQIDLTLtQdASMG1vvXgZjBUa-H8",
+//     authDomain: "meditation-app-9f8f1.firebaseapp.com",
+//     projectId: "meditation-app-9f8f1",
+//     storageBucket: "meditation-app-9f8f1.appspot.com",
+//     messagingSenderId: "456537085007",
+//     appId: "1:456537085007:web:41b5aeb2598ed5ed82063a"
+// };
+// // Initialize Firebase
+// firebase.initializeApp(firebaseConfig);
+// const db = firebase.firestore(); //add this to read/write
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyBAyDCM45wWDnFELbXSYZw84uh30Ysw478",
+      authDomain: "march02demo-1469c.firebaseapp.com",
+      projectId: "march02demo-1469c",
+      storageBucket: "march02demo-1469c.appspot.com",
+      messagingSenderId: "661947135866",
+      appId: "1:661947135866:web:f60acace8895913f8448c3",
+      measurementId: "G-JN3DC2Q7L6"
+  };
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  const db = firebase.firestore(); //add this to read/write
--- scripts/reminder.js
+++ scripts/reminder.js
@@ -12,3 +12,90 @@ $(document).ready(function() {
         }
     });
 });
+
+function convertDaysToString(days) {
+    var result = [];
+    days.sort();
+    days.forEach(element => {
+        result.push(convertDayShort(element));
+    });
+    return result.join(".");
+}
+
+function convertDayShort(day) {
+    var short;
+    switch(day) {
+        case 0:
+            short = "M";
+            break;
+        case 1:
+            short = "T";
+            break;
+        case 2:
+            short = "W";
+            break;
+        case 3:
+            short = "Th";
+            break;
+        case 4:
+            short = "F";
+            break;
+        case 5:
+            short = "Sat";
+            break;
+        case 6:
+            short = "Sun";
+            break;
+        default:
+            short = "";    
+    }
+    return short;
+}
+
+function convertDaysToArray(days) {
+    var result = [];
+    days.sort();
+    days.forEach(element => {
+        result.push(convertDay(element));
+    });
+    return result;
+}
+
+function convertDay(day) {
+    var short;
+    switch(day) {
+        case 0:
+            short = "mon";
+            break;
+        case 1:
+            short = "tues";
+            break;
+        case 2:
+            short = "wed";
+            break;
+        case 3:
+            short = "thurs";
+            break;
+        case 4:
+            short = "fri";
+            break;
+        case 5:
+            short = "sat";
+            break;
+        case 6:
+            short = "sun";
+            break;
+        default:
+            short = "";    
+    }
+    return short;
+}
+
+function remove(arr, element) {
+    const index = arr.indexOf(element);
+    if(index > -1) {
+        arr.splice(index, 1);
+    }
+}
+
+
